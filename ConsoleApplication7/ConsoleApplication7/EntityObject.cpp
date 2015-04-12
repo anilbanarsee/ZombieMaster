29cf0d3b8645d96ae9ae279defb8a03b732dc5d5
anilbanarsee <anil.banarsee@googlemail.com>
2015-04-12 01:53:49 +0100

    modified:   ConsoleApplication7/ConsoleApplication7/EntityObject.cpp 	modified:   ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.vcxproj 	modified:   ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.vcxproj.filters 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ConsoleApplication7.log 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/Source.obj 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/CL.read.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/CL.write.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/cl.command.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.command.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.read.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.write.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/vc120.idb 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/vc120.pdb 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.exe 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.ilk 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.pdb 	ConsoleApplication7/ConsoleApplication7/Debug/Projectile.obj 	ConsoleApplication7/ConsoleApplication7/Projectile.cpp 	ConsoleApplication7/ConsoleApplication7/Projectile.h 	ConsoleApplication7/ConsoleApplication7/test.png 	ConsoleApplication7/ZombieMasterProject.opensdf 	ConsoleApplication7/ZombieMasterProject.sdf 	ConsoleApplication7/ZombieMasterProject.v12.suo

--- ConsoleApplication7/ConsoleApplication7/EntityObject.cpp
+++ ConsoleApplication7/ConsoleApplication7/EntityObject.cpp
@@ -1,5 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include "EntityObject.h"
+EntityObject::EntityObject()
+{
+
+}
 EntityObject::EntityObject(float posXIn,float posYIn,std::string spriteFileName)
 {
 	setPosition(posXIn, posYIn);
