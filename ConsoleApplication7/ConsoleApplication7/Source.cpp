4b002705920973fb307ba7c0f86fa7e380634260
anilbanarsee <anil.banarsee@googlemail.com>
2015-04-12 01:49:54 +0100

    modified:   ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.vcxproj 	modified:   ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.vcxproj.filters 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ConsoleApplication7.log 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/Source.obj 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/CL.read.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/CL.write.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/cl.command.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.command.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.read.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/ZombieMa.35DF18B8.tlog/link.write.1.tlog 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/vc120.idb 	modified:   ConsoleApplication7/ConsoleApplication7/Debug/vc120.pdb 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.exe 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.ilk 	modified:   ConsoleApplication7/Debug/ZombieMasterProject.pdb 	ConsoleApplication7/ConsoleApplication7/Debug/Projectile.obj 	ConsoleApplication7/ConsoleApplication7/Projectile.cpp 	ConsoleApplication7/ConsoleApplication7/Projectile.h 	ConsoleApplication7/ConsoleApplication7/test.png

--- ConsoleApplication7/ConsoleApplication7/Source.cpp
+++ ConsoleApplication7/ConsoleApplication7/Source.cpp
@@ -2,20 +2,23 @@
 #include <iostream>
 #include <Windows.h>
 #include <cmath>
-#include "EntityObject.h"
+#include "Projectile.h"
 #include <vector>
- 
+
 using sf::Vector2;
+using sf::Vector2f;
 using std::vector;
 using std::cout;
 using std::endl;
 
-bool hit(Vector2<float> v1, Vector2<float> v2, double r1, double r2){
-	
-	
-	
+#define PI 3.14159265
+
+bool hit(Vector2f v1, Vector2f v2, double r1, double r2){
+
+
+
 	double comR = r1 + r2;
-	
+
 
 	double xpos1 = v1.x;
 	double xpos2 = v2.x;
@@ -26,7 +29,7 @@ bool hit(Vector2<float> v1, Vector2<float> v2, double r1, double r2){
 	double diffX = xpos1 - xpos2;
 	double diffY = ypos1 - ypos2;
 
-
+	atan(1);
 	diffX = diffX*diffX;
 	diffY = diffY*diffY;
 
@@ -37,29 +40,29 @@ bool hit(Vector2<float> v1, Vector2<float> v2, double r1, double r2){
 	}
 	return false;
 }
-float length(Vector2<float> v1){
+float length(Vector2f v1){
 	return sqrt((v1.x*v1.x) + (v1.y*v1.y));
 }
 int main()
 {
 
 	double player2Power = 0.01;
-	double bulletSpeed = 0.05;
+	double bulletSpeed = 0.5;
 
-	int bulletRate = 3000;
+	int bulletRate = 100;
 	int bulletCounter = 0;
 
 	int WIDTH = 600;
 	int HEIGHT = 600;
 
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "ZombieMaster");
-	double power = 0.0000005;
-	double friction = 0.0;
+	double power = 0.005;
+	double friction = 0.0025;
 
 	double topSpeed = 0.5;
 	double xSpeed = 0;
 	double ySpeed = 0;
-
+	
 	float locX = 0;
 	float locY = 0;
 
@@ -75,18 +78,23 @@ int main()
 
 	sf::CircleShape player(10.f);
 	sf::CircleShape player2(10.f);
-	
-	vector<sf::CircleShape*> bullets;
-	vector<Vector2<float>> bulletMovement;
+	player.setOrigin(player.getRadius(), player.getRadius());
+	Vector2f move;
+	move.x = 0;
+	move.y = 0;
+	Projectile object(300, 300, "test.png", move);
+
+	vector<Projectile*> bullets;
+	vector<Vector2f> bulletMovement;
 
 	player2.setPosition(WIDTH / 2, HEIGHT / 2);
 	player.setPosition(300, 400);
-	
+
 
 	while (window.isOpen()){
-
+		Sleep(1);
 		sf::Event event;
-		
+
 		while (window.pollEvent(event)){
 
 
@@ -94,17 +102,17 @@ int main()
 			if (event.type == sf::Event::Closed){
 				window.close();
 			}
-		
+
 			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right){
 				sf::Vector2i c = sf::Mouse::getPosition(window);
-				locX = (float) c.x;
-				locY = (float) c.y;
+				locX = (float)c.x;
+				locY = (float)c.y;
 
-				sf::Vector2f target(locX,locY);
-				Vector2<float> current = player2.getPosition();
+				sf::Vector2f target(locX, locY);
+				Vector2f current = player2.getPosition();
 				current.x = current.x + player2.getRadius();
 				current.y = current.y + player2.getRadius();
-				Vector2<float> movement = (target - current);
+				Vector2f movement = (target - current);
 				movement = movement / length(movement);
 				movement.x = movement.x*player2Power;
 				movement.y = movement.y*player2Power;
@@ -116,233 +124,233 @@ int main()
 				Vector2<float> f = player2.getPosition();
 				float x = f.x;
 				float y = f.y;
-
-
-
 				float diffx = locX - x;
 				float diffy = locY - y;
 
-				
-
 				float t = (diffx*diffx + diffy*diffy) / (float) player2Power;
-
 				cout << t << endl;
 				cout << diffx << endl;
 				cout << diffy << endl;
-
 				xs = (diffx*diffx)/t;
 				if (diffx < 0){
-					xs = -xs;
+				xs = -xs;
 				}
 				ys = (diffy*diffy)/t;
 				if (diffy < 0){
-					ys = -ys;
+				ys = -ys;
 				}
-
 				cout << "Mouse x :" << locX;
 				cout << "" << endl;
 				cout << "Mouse y :" << locY;
 				cout << "" << endl;
-
 				cout << "Object x :" << x;
 				cout << "" << endl;
 				cout << "Object y :" << y;
 				cout << "" << endl;
-
 				cout << "Speed x :" << xs;
 				cout << "" << endl;
 				cout << "Speed y :" << ys;
 				cout << "" << endl;*/
 
 			}
-		
+
 		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+
+			xSpeed -= power;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+
+			xSpeed -= power;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+
+			xSpeed += power;
+
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+
+			xSpeed += power;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+
+			ySpeed -= power;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+
+			ySpeed -= power;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+
+			ySpeed += power;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+
+			ySpeed += power;
+		}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && bulletCounter == 0){
+			
+	
+			Vector2f current = player.getPosition();
+
+			
+			
+
+			sf::Vector2i c = sf::Mouse::getPosition(window);
+			locX = (float)c.x;
+			locY = (float)c.y;
+
+			sf::Vector2f target(locX, locY);
+
+			Vector2f movement = (target - current);
+			movement = movement / length(movement);
+			movement.x = movement.x*bulletSpeed;
+			movement.y = movement.y*bulletSpeed;
+			
 		
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-
-				xSpeed -= power;
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-
-				xSpeed -= power;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-
-				xSpeed += power;
-
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-				
-				xSpeed += power;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-
-				ySpeed -= power;
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-
-				ySpeed -= power;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-
-				ySpeed += power;
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-
-				ySpeed += power;
-			}
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)&&bulletCounter==0){
-				sf::CircleShape  *bullet = new sf::CircleShape(2.f);
-				
-				Vector2<float> current = player.getPosition();
-				current.x = current.x + player.getRadius();
-				current.y = current.y + player.getRadius();
-				(*bullet).setPosition(current);
-				sf::Vector2i c = sf::Mouse::getPosition(window);
-				locX = (float)c.x;
-				locY = (float)c.y;
-
-				sf::Vector2f target(locX, locY);
-
-				Vector2<float> movement = (target - current);
-				movement = movement / length(movement);
-				movement.x = movement.x*bulletSpeed;
-				movement.y = movement.y*bulletSpeed;
-				bulletMovement.push_back(movement);
-			
-				bullets.push_back(bullet);
-				bulletCounter = bulletRate;
-			}
-
-			if (xSpeed >= 0){
-				xSpeed -= friction;
-				if (xSpeed < 0){
-					xSpeed = 0;
-				}
-			}
-			if (xSpeed <= 0){
-				xSpeed += friction;
-				if (xSpeed > 0){
-					xSpeed = 0;
-				}
-			}
-			if (ySpeed >= 0){
-				ySpeed -= friction;
-				if (ySpeed < 0){
-					ySpeed = 0;
-				}
-			}
-			if (ySpeed <= 0){
-				ySpeed += friction;
-				if (ySpeed > 0){
-					ySpeed = 0;
-				}
-			}
-
-			if (xSpeed >= topSpeed){
-				xSpeed = topSpeed;
-			}
-			if (xSpeed <= -topSpeed){
-				xSpeed = -topSpeed;
-			}
-			if (ySpeed >= topSpeed){
-				ySpeed = topSpeed;
-			}
-			if (ySpeed <= -topSpeed){
-				ySpeed = -topSpeed;
-			}
-			
-			
 
 			
 
-			if (movingTowards){
-				
-				Vector2<float> c = player2.getPosition();
-				double r = player2.getRadius();
-				
-				if ((c.x+r<locX + 1 && c.x+r>locX - 1) && (c.y+r<locY + 1 && c.y+r>locY - 1)){
-					movingTowards = false;
-				}
 
-				//cout << diffx << endl;
-				//cout << diffy << endl;
-				//cout << "ALERT" << endl;
-				//movingTowards = false;
 
-				
-				
+			//cout << sin(radians) * distance << "," << cos(radians)*distance << endl;
+
+			Projectile  *bullet = new Projectile(current.x, current.y, "test.png",movement);
+
+			bullets.push_back(bullet);
+			bulletCounter = bulletRate;
+		}
+
+		if (xSpeed >= 0){
+			xSpeed -= friction;
+			if (xSpeed < 0){
+				xSpeed = 0;
+			}
+		}
+		if (xSpeed <= 0){
+			xSpeed += friction;
+			if (xSpeed > 0){
+				xSpeed = 0;
+			}
+		}
+		if (ySpeed >= 0){
+			ySpeed -= friction;
+			if (ySpeed < 0){
+				ySpeed = 0;
+			}
+		}
+		if (ySpeed <= 0){
+			ySpeed += friction;
+			if (ySpeed > 0){
+				ySpeed = 0;
+			}
+		}
+
+		if (xSpeed >= topSpeed){
+			xSpeed = topSpeed;
+		}
+		if (xSpeed <= -topSpeed){
+			xSpeed = -topSpeed;
+		}
+		if (ySpeed >= topSpeed){
+			ySpeed = topSpeed;
+		}
+		if (ySpeed <= -topSpeed){
+			ySpeed = -topSpeed;
+		}
+
+
+
+
+
+		if (movingTowards){
+
+			Vector2f c = player2.getPosition();
+			double r = player2.getRadius();
+
+			if ((c.x + r<locX + 1 && c.x + r>locX - 1) && (c.y + r<locY + 1 && c.y + r>locY - 1)){
+				movingTowards = false;
+			}
+
+			//cout << diffx << endl;
+			//cout << diffy << endl;
+			//cout << "ALERT" << endl;
+			//movingTowards = false;
+
+
+
+		}
+		else{
+			xs = 0;
+			ys = 0;
+		}
+
+
+		Vector2f c = player.getPosition();
+
+		
+
+		float px = c.x;
+		float py = c.y;
+
+		if (px >= WIDTH && xSpeed > 0){
+			xSpeed = 0;
+		}
+		if (px <= 0 && xSpeed < 0){
+			xSpeed = 0;
+		}
+
+		if (py >= HEIGHT && ySpeed > 0){
+			ySpeed = 0;
+		}
+		if (py <= 0 && ySpeed < 0){
+			ySpeed = 0;
+		}
+		player.move(xSpeed, ySpeed);
+		//player2.setPosition(locX, locY);
+		player2.move(xs, ys);
+
+		vector<Projectile*>::iterator it;
+		for (it = bullets.begin(); it != bullets.end();){
+
+			if (((*it)->getPosition().x > WIDTH || (*it)->getPosition().x<0) || ((*it)->getPosition().y>HEIGHT || (*it)->getPosition().y < 0)){
+				delete * it;
+				it = bullets.erase(it);
 			}
 			else{
-				xs = 0;
-				ys = 0;
+				(*it)->incr();
+				++it;
 			}
+			//sf::CircleShape tempBullet = (*bullets.at(i));
+			//if ((tempBullet.getPosition().x > WIDTH || tempBullet.getPosition().x<0) || (tempBullet.getPosition().y>HEIGHT || tempBullet.getPosition().y < 0)){
+
+			//}
+			//
+			//cout << bulletMovement.at(i).x << endl;
+			//bullets.at(i).setPosition()
+		}
+		if (bulletCounter > 0){
+			bulletCounter--;
+		}
+		//cout << movingTowards << endl;
+
+		//cout << bulletCounter << endl;
+		//cout << bullets.size() << endl;
+
+		window.clear();
+		for (int i = 0; i < bullets.size(); i++){
+			window.draw((*bullets.at(i)));
+		}
+		window.draw(player2);
+		window.draw(player);
+
 
 		
-			Vector2<float> c = player.getPosition();
-			
-			double r = player.getRadius();
-
-			float px = c.x + r;
-			float py = c.y + r;
-
-			if (px >= WIDTH && xSpeed > 0){
-				xSpeed = 0;
-			}
-			if (px <= 0 && xSpeed < 0){
-				xSpeed = 0;
-			}
-
-			if (py >= HEIGHT && ySpeed > 0){
-				ySpeed = 0;
-			}
-			if (py <= 0 && ySpeed < 0){
-				ySpeed = 0;
-			}
-			player.move(xSpeed, ySpeed);
-			//player2.setPosition(locX, locY);
-			player2.move(xs, ys);
-
-			vector<sf::CircleShape*>::iterator it;
-			for (it = bullets.begin(); it != bullets.end();){
-				
-				if (((*it)->getPosition().x > WIDTH || (*it)->getPosition().x<0) || ((*it)->getPosition().y>HEIGHT || (*it)->getPosition().y < 0)){
-					delete * it;
-					it = bullets.erase(it);
-				}
-				else{
-					(*it)->move(0.05,0.05);
-					++it;
-				}
-				//sf::CircleShape tempBullet = (*bullets.at(i));
-				//if ((tempBullet.getPosition().x > WIDTH || tempBullet.getPosition().x<0) || (tempBullet.getPosition().y>HEIGHT || tempBullet.getPosition().y < 0)){
-					
-				//}
-				//
-				//cout << bulletMovement.at(i).x << endl;
-				//bullets.at(i).setPosition()
-			}
-			if (bulletCounter > 0){
-				bulletCounter--;
-			}
-			//cout << movingTowards << endl;
-			
-			//cout << bulletCounter << endl;
-			//cout << bullets.size() << endl;
-
-			window.clear();
-			for (int i = 0; i < bullets.size(); i++){
-				window.draw((*bullets.at(i)));
-			}
-			window.draw(player2);
-			window.draw(player);
+		window.draw(object);
 
 
-			EntityObject object(300, 300, "C:/Users/Hamzah/OneDrive/ZombieMaster/ConsoleApplication7/ConsoleApplication7/Data/test.png");
-			window.draw(object);
-
-
-			window.display();
+		window.display();
 	}
 	return 0;
 }
